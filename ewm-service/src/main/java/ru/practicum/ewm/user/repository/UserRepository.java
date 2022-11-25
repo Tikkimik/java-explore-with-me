79d2994723e71d1f9b1fb3ae4d8dc85a7adeaf31
Tikkimik <googlbubu@gmail.com>
2022-11-25 20:42:37 +0300

    admin get user

--- ewm-service/src/main/java/ru/practicum/ewm/user/repository/UserRepository.java
+++ ewm-service/src/main/java/ru/practicum/ewm/user/repository/UserRepository.java
@@ -1,7 +1,14 @@
 package ru.practicum.ewm.user.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import ru.practicum.ewm.user.model.User;
 
+import java.util.List;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    @Query(value = "SELECT user FROM User user WHERE user.id IN :userIds")
+    List<User> findUsersByIds(List<Long> userIds);
+
 }
--- ewm-service/src/main/java/ru/practicum/ewm/user/service/UserServiceImpl.java
+++ ewm-service/src/main/java/ru/practicum/ewm/user/service/UserServiceImpl.java
@@ -4,9 +4,11 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import ru.practicum.ewm.user.dto.CreateUserDto;
 import ru.practicum.ewm.user.dto.UserDto;
+import ru.practicum.ewm.user.mapper.UserMapper;
 import ru.practicum.ewm.user.repository.UserRepository;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static ru.practicum.ewm.user.mapper.UserMapper.toUser;
 import static ru.practicum.ewm.user.mapper.UserMapper.toUserDto;
@@ -24,7 +26,10 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public List<UserDto> findUsersByIds(List<Long> userIds) {
-        return null;
+        return userRepository.findUsersByIds(userIds)
+                .stream()
+                .map(UserMapper::toUserDto)
+                .collect(Collectors.toList());
     }
 
     @Override
